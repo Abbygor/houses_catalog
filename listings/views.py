ef3ef7ed56417dd8e48a180fc3671c6ee2d3eddf
Luis Antonio Feregrino <djlaf2008@hotmail.com>
2019-11-12 22:48:46 -0600

    Load dynamic data into SearchPage, populate the select fields with the data stored in the dictionaries, and get the data from database filtering by the fields in the search bar

--- listings/views.py
+++ listings/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404,render
 from .models import Listing
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from .choices import prize_choices, bedroom_choices, state_choices
+
 # Create your views here.
 def index(request):
     
@@ -24,4 +26,44 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    queryset_list = Listing.objects.order_by('-list_date')
+    #Keywords
+    if 'keywords' in request.GET:#name of the field keywords
+        keywords = request.GET['keywords']
+        if keywords:#not empty string
+            queryset_list = queryset_list.filter(description__icontains=keywords)#fieldtosearch__(double underscore)icontains
+
+    #City
+    if 'city' in request.GET:#name of the field city
+        city = request.GET['city']
+        if city:#not empty string
+            queryset_list = queryset_list.filter(city__iexact=city)#fieldtosearch__(double underscore)iexact
+
+    #State
+    if 'state' in request.GET:#name of the field state
+        state = request.GET['state']
+        if state:#not empty string
+            queryset_list = queryset_list.filter(state__iexact=state)#fieldtosearch__(double underscore)iexact
+    
+    #Bedrooms
+    if 'bedrooms' in request.GET:#name of the field bedrooms
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:#not empty string
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)#fieldtosearch__(double underscore)iexact
+
+    #Price
+    if 'price' in request.GET:#name of the field price
+        price = request.GET['price']
+        if price:#not empty string
+            queryset_list = queryset_list.filter(price__lte=price)#fieldtosearch__(double underscore)iexact
+
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'prize_choices': prize_choices,
+        'listings': queryset_list
+    }
+
+    return render(request, 'listings/search.html', context)
--- pages/views.py
+++ pages/views.py
@@ -4,11 +4,17 @@ from django.http import HttpResponse
 from listings.models import Listing
 from realtors.models import Realtor
 
+from listings.choices import prize_choices, bedroom_choices, state_choices
+
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
     context = {
-        'listings': listings
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'prize_choices': prize_choices
+
     }
 
     return render(request, 'pages/index.html',context)
