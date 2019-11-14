b6d04da0693706c73f8e0e6a940516ae1ac42ebf
Luis Antonio Feregrino <djlaf2008@hotmail.com>
2019-11-13 22:08:02 -0600

    AccountsApp added to managen the login and register functions

--- houses/settings.py
+++ houses/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.app.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
